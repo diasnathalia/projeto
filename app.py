c684570a2d2297a1198f79df7b4dbc023ce5f7aa
diasnathalia <dias.nathalia14@escola.pr.gov.br>
2025-05-26 08:04:39 -0300

    g

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
 def login():
     username = request.form['username']
     password = request.form['password']
-   
+    
     if username == 'gabriel@' and password == '123':
         return redirect('/servicos')
     else:
@@ -27,5 +27,5 @@ def servicos():
     return render_template('servicos.html')
 
 
-if __name__ == '_main_':
+if __name__ == '__main__':
     app.run(debug=True)
